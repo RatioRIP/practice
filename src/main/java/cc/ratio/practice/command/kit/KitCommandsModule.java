7761adbcd12d92f28124407a26ac3c3f8ed1867d
pcranaway <pcranaway@tuta.io>
2022-03-03 15:04:55 +0200

    add setkitbuild and setkitranked command

--- src/main/java/cc/ratio/practice/command/kit/KitCommandsModule.java
+++ src/main/java/cc/ratio/practice/command/kit/KitCommandsModule.java
@@ -1,9 +1,6 @@
 package cc.ratio.practice.command.kit;
 
-import cc.ratio.practice.command.kit.handler.DeleteKitCommandHandler;
-import cc.ratio.practice.command.kit.handler.NewKitCommandHandler;
-import cc.ratio.practice.command.kit.handler.SetKitDisplayHandler;
-import cc.ratio.practice.command.kit.handler.SetKitInventoryHandler;
+import cc.ratio.practice.command.kit.handler.*;
 import me.lucko.helper.Commands;
 import me.lucko.helper.terminable.TerminableConsumer;
 import me.lucko.helper.terminable.module.TerminableModule;
@@ -41,5 +38,18 @@ public class KitCommandsModule implements TerminableModule {
                 .handler(new SetKitDisplayHandler())
                 .registerAndBind(consumer, "setkitdisplay");
 
+        Commands.create()
+                .assertPlayer()
+                .assertUsage("<name> [value]")
+                .description("Sets value of the build option of a kit")
+                .handler(new SetKitBuildHandler())
+                .registerAndBind(consumer, "setkitbuild");
+
+        Commands.create()
+                .assertPlayer()
+                .assertUsage("<name> [value]")
+                .description("Sets value of the ranked option of a kit")
+                .handler(new SetKitRankedHandler())
+                .registerAndBind(consumer, "setkitranked");
     }
 }
---
+++ src/main/java/cc/ratio/practice/command/kit/handler/SetKitBuildHandler.java
@@ -0,0 +1,44 @@
+package cc.ratio.practice.command.kit.handler;
+
+import cc.ratio.practice.kit.Kit;
+import cc.ratio.practice.kit.KitRepository;
+import me.lucko.helper.Services;
+import me.lucko.helper.command.CommandInterruptException;
+import me.lucko.helper.command.argument.Argument;
+import me.lucko.helper.command.context.CommandContext;
+import me.lucko.helper.command.functional.FunctionalCommandHandler;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
+
+public class SetKitBuildHandler implements FunctionalCommandHandler<Player> {
+
+    public final KitRepository kitRepository = Services.get(KitRepository.class).get();
+
+    @Override
+    public void handle(CommandContext<Player> c) throws CommandInterruptException {
+        String name = c.arg(0).parseOrFail(String.class);
+
+        Optional<Kit> optional = kitRepository.find(name);
+
+        if(!optional.isPresent()) {
+            c.reply("&cKit doesn't exist");
+            return;
+        } else {
+            Kit kit = optional.get();
+
+            boolean value = !kit.build;
+
+            Argument valueArgument = c.arg(1);
+
+            if(valueArgument.isPresent()) {
+                value = valueArgument.parseOrFail(Boolean.class);
+            }
+
+            kit.build = value;
+
+            c.reply("Option &ebuild &r" + (kit.build ? "&aenabled" : "&cdisabled"));
+            c.reply("Kit '" + kit.name + "' modified");
+        }
+    }
+}

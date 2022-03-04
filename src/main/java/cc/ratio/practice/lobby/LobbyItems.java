e1eae921f0173ab80b3c6915ce9cbe0d6280d85c
pcranaway <pcranaway@tuta.io>
2022-03-04 15:28:04 +0200

    fix typos

--- src/main/java/cc/ratio/practice/lobby/LobbyItems.java
+++ src/main/java/cc/ratio/practice/lobby/LobbyItems.java
@@ -11,15 +11,15 @@ public class LobbyItems {
 
     public static final ItemStack UNRANKED_QUEUE_ITEM = ItemStackBuilder.of(Material.IRON_SWORD)
             .name("&7Unranked Queue")
-            .lore("&7Rick click to open the Unranked Queue menu")
+            .lore("&7Right click to open the Unranked Queue menu")
             .build();
 
     public static final ItemStack RANKED_QUEUE_ITEM = ItemStackBuilder.of(Material.DIAMOND_SWORD)
-            .name("&cUnranked Queue")
-            .lore("&7Rick click to open the Ranked Queue menu")
+            .name("&cRanked Queue")
+            .lore("&7Right click to open the Ranked Queue menu")
             .build();
 
-    public static final Map<Integer, ItemStack> ITEMS = new HashMap<>() {{
+    public static final Map<Integer, ItemStack> ITEMS = new HashMap<Integer, ItemStack>() {{
         put(0, UNRANKED_QUEUE_ITEM);
         put(1, RANKED_QUEUE_ITEM);
     }};

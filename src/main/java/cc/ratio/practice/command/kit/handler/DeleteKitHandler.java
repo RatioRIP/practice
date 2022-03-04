462a300cdd66929a38f1e48011d25e559f0c7d1e
pcranaway <pcranaway@tuta.io>
2022-03-04 16:18:02 +0200

    rename deletekit and newkit commands

--- src/main/java/cc/ratio/practice/command/kit/handler/DeleteKitHandler.java
+++ src/main/java/cc/ratio/practice/command/kit/handler/DeleteKitHandler.java
@@ -6,12 +6,11 @@ import me.lucko.helper.Services;
 import me.lucko.helper.command.CommandInterruptException;
 import me.lucko.helper.command.context.CommandContext;
 import me.lucko.helper.command.functional.FunctionalCommandHandler;
-import me.lucko.helper.item.ItemStackBuilder;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
-public class NewKitCommandHandler implements FunctionalCommandHandler<Player> {
+import java.util.Optional;
+
+public class DeleteKitHandler implements FunctionalCommandHandler<Player> {
 
     public final KitRepository kitRepository = Services.get(KitRepository.class).get();
 
@@ -19,19 +18,15 @@ public class NewKitCommandHandler implements FunctionalCommandHandler<Player> {
     public void handle(CommandContext<Player> c) throws CommandInterruptException {
         String name = c.arg(0).parseOrFail(String.class);
 
-        if(kitRepository.find(name).isPresent()) {
-            c.reply("&cKit already exists");
+        Optional<Kit> optional = kitRepository.find(name);
+
+        if(!optional.isPresent()) {
+            c.reply("&cKit doesn't exist");
             return;
         } else {
-            Kit kit = new Kit(name);
-
-            kit.contents = new ItemStack[]{};
-            kit.armor = new ItemStack[]{};
-            kit.display = ItemStackBuilder.of(Material.DEAD_BUSH).build();
-            kit.build = false;
-
-            kitRepository.put(kit);
-            c.reply("Created Kit '" + name + "'");
+            Kit kit = optional.get();
+            kitRepository.remove(kit);
+            c.reply("Deleted Kit '" + name + "'");
         }
     }
 }
--- src/main/java/cc/ratio/practice/command/kit/handler/NewKitHandler.java
+++ src/main/java/cc/ratio/practice/command/kit/handler/NewKitHandler.java
@@ -6,11 +6,12 @@ import me.lucko.helper.Services;
 import me.lucko.helper.command.CommandInterruptException;
 import me.lucko.helper.command.context.CommandContext;
 import me.lucko.helper.command.functional.FunctionalCommandHandler;
+import me.lucko.helper.item.ItemStackBuilder;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
-import java.util.Optional;
-
-public class DeleteKitCommandHandler implements FunctionalCommandHandler<Player> {
+public class NewKitHandler implements FunctionalCommandHandler<Player> {
 
     public final KitRepository kitRepository = Services.get(KitRepository.class).get();
 
@@ -18,15 +19,19 @@ public class DeleteKitCommandHandler implements FunctionalCommandHandler<Player>
     public void handle(CommandContext<Player> c) throws CommandInterruptException {
         String name = c.arg(0).parseOrFail(String.class);
 
-        Optional<Kit> optional = kitRepository.find(name);
-
-        if(!optional.isPresent()) {
-            c.reply("&cKit doesn't exist");
+        if(kitRepository.find(name).isPresent()) {
+            c.reply("&cKit already exists");
             return;
         } else {
-            Kit kit = optional.get();
-            kitRepository.remove(kit);
-            c.reply("Deleted Kit '" + name + "'");
+            Kit kit = new Kit(name);
+
+            kit.contents = new ItemStack[]{};
+            kit.armor = new ItemStack[]{};
+            kit.display = ItemStackBuilder.of(Material.DEAD_BUSH).build();
+            kit.build = false;
+
+            kitRepository.put(kit);
+            c.reply("Created Kit '" + name + "'");
         }
     }
 }
